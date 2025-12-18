eee5e5d92078fd8b33d1766658140ab92bfcfda0
eureka <eureka@example.com>
2025-12-18 18:29:39 +0100

    sql da badań: podzial na części prod i test

--- src/eu_async_llm.sql
+++ src/eu_async_llm.sql
@@ -1,47 +1,27 @@
 -- Run once
 -- CREATE EXTENSION IF NOT EXISTS vector;
 -------------------------------------------------------------------------
--- qa_result_id BIGINT NOT NULL REFERENCES qa_results(id) !!!!!!!!!!!!!!
+-- qa_result_id BIGINT NOT NULL REFERENCES qa_results(id) ON DELETE CASCADE !!!!!!!!!!!!!!
 
+
+-------------------------------------------------------------------------
+--------------------- PROD ----------------------------------------------
+-------------------------------------------------------------------------
+-------------------------------------------------------------------------
+-- MAIN TABLES
+-------------------------------------------------------------------------
+DROP TABLE qa_results CASCADE
 DROP TABLE answer_embeddings_qa
-DROP TABLE answer_embeddings_sa
-DROP TABLE qa_results --CASCADE
 DROP TABLE stat_results
 
-TRUNCATE TABLE qa_results CASCADE
+TRUNCATE TABLE qa_results_test CASCADE
 TRUNCATE TABLE answer_embeddings_qa
-TRUNCATE TABLE answer_embeddings_sa
 TRUNCATE TABLE stat_results
 
+DROP TABLE answer_embeddings_sa
+TRUNCATE TABLE answer_embeddings_sa
 
---TRUNCATE qa_results RESTART IDENTITY
-
-
-
-
-DELETE FROM qa_results
-WHERE text_id = 669825
-
---DELETE FROM answer_embeddings
---WHERE text_id = 669935
-
-
--------------------------------------------------------------------------
--- Statistics
--------------------------------------------------------------------------
-CREATE TABLE IF NOT EXISTS stat_results (
-  id BIGSERIAL PRIMARY KEY,
-  text_id INTEGER NOT NULL,
-  tax_type TEXT NOT NULL,
-  type_results TEXT NOT NULL, -- a piece of python code
-  results JSON NOT NULL, -- in general timings in seconds !!! (_s)
-  created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL,
-  updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL
-);
-
-
--------------------------------------------------------------------------
--- QA results per chunk/question/model
+-- PROD: MAIN TABLE!!! QA results per chunk/question/model
 CREATE TABLE IF NOT EXISTS qa_results (
   id BIGSERIAL NOT NULL PRIMARY KEY,
   text_id INTEGER NOT NULL, -- id_informacji
@@ -60,11 +40,10 @@ CREATE TABLE IF NOT EXISTS qa_results (
   updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL 
 );
 
--------------------------------------------------------------------------
--- Embeddings of answers for retrieval/RAG
+-- PROD: Embeddings of answers for retrieval/RAG
 CREATE TABLE IF NOT EXISTS answer_embeddings_qa (
   id BIGSERIAL NOT NULL PRIMARY KEY,
-  qa_results_id BIGINT NOT NULL REFERENCES qa_results(id) ON DELETE CASCADE,
+  qa_results_id BIGINT NOT NULL REFERENCES qa_results_test(id) ON DELETE CASCADE,
   text_id INTEGER NOT NULL,
   tax_type TEXT NOT NULL,
   question_id SMALLINT NOT NULL,
@@ -75,6 +54,25 @@ CREATE TABLE IF NOT EXISTS answer_embeddings_qa (
   created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL,
   updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
+
+-- PROD: Statistics
+CREATE TABLE IF NOT EXISTS stat_results (
+  id BIGSERIAL PRIMARY KEY,
+  text_id INTEGER NOT NULL,
+  tax_type TEXT NOT NULL,
+  type_results TEXT NOT NULL, -- a piece of python code
+  results JSON NOT NULL, -- in general timings in seconds !!! (_s)
+  created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL,
+  updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL
+);
+
+-------------------------------------------------------------------------
+-- PROD: OPTIONAL (without REFERENCES qa_results(id) ON DELETE CASCADE)
+-- CHANGE sa to vat, cit, pit ?????
+-- GOAL is SPLITED BY tax_type AND INSERT INTO answer_embeddings_qa
+-------------------------------------------------------------------------
+-- DROP TABLE answer_embeddings_sa
+-- TRUNCATE TABLE answer_embeddings_sa
 
 CREATE TABLE IF NOT EXISTS answer_embeddings_sa (
   id BIGSERIAL NOT NULL PRIMARY KEY,
@@ -90,33 +88,53 @@ CREATE TABLE IF NOT EXISTS answer_embeddings_sa (
   updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
 
-
-
-
 -------------------------------------------------------------------------
+-- OPTION
+-------------------------------------------------------------------------
+--DELETE FROM qa_results --(CASCADE works)
+--WHERE text_id = 669825
+
+--TRUNCATE qa_results RESTART IDENTITY
+
 --CREATE INDEX IF NOT EXISTS idx_answer_embeddings_cosine
 --  ON answer_embeddings USING ivfflat (embedding vector_cosine_ops) WITH (lists = 100);
 --ANALYZE answer_embeddings;
 -------------------------------------------------------------------------
 
 
-SELECT * FROM qa_results 
-SELECT * FROM answer_embeddings_qa
-SELECT * FROM answer_embeddings_sa
-SELECT * FROM stat_results
+-------------------------------------------------------------------------
+-- SQL TESTS
+-------------------------------------------------------------------------
+--- SELECT
 
+SELECT * FROM qa_results ORDER BY id DESC
+SELECT * FROM answer_embeddings_qa ORDER BY id DESC
+SELECT * FROM stat_results ORDER BY id DESC
+SELECT * FROM answer_embeddings_sa ORDER BY id DESC 
 
 
 SELECT 
-text_id,
-tax_type,
-json_extract_path_text(results,'chunks') AS chunks, 
-json_extract_path_text(results,'text_norm_len') AS text_norm_len ,
-json_extract_path_text(results,'timings', 'llm_latency_s') AS llm_latency_s ,
-json_extract_path_text(results,'timings', 'total_s') AS total_s  
+    text_id,
+    tax_type,
+    type_results,
+    json_extract_path_text(results,'chunks') AS chunks, 
+    json_extract_path_text(results,'text_norm_len') AS text_norm_len ,
+    json_extract_path_text(results,'timings', 'llm_latency_s') AS llm_latency_s ,
+    json_extract_path_text(results,'timings', 'total_s') AS total_s  
 FROM stat_results
 
 
+-- INSERT INTO answer_embeddings_sa_test
+WITH t0 AS (
+SELECT * FROM answer_embeddings_qa_test
+WHERE type_text='chunk' 
+)
+INSERT INTO answer_embeddings_sa_test
+SELECT * FROM t0 AS ta
+WHERE qa_results_id = ANY(SELECT MIN(qa_results_id) FROM t0 AS tb WHERE ta.text_id = tb.text_id)
+
+-------------------------------------------------------------------------
+-- EXAMPLE JSON
 -------------------------------------------------------------------------
 {"text_id": 669935, "tax_type": "vat", "chunks": 5, "text_norm_len": 68743, "questions_per_context": 14, 
 "llm_models": ["hf.co/NikolayKozloff/Llama-PLLuM-8B-instruct-Q8_0-GGUF:Q8_0"], "embed_models": "/dane/models/sdadas/stella-pl-retrieval", "embed_dim": 1024, "asyncio_semaphore": 10, "qa_results_saved": 70, 
@@ -124,12 +142,8 @@ FROM stat_results
 {"text_id": 669933, "tax_type": "pit", "chunk_id": 1, "question_id": 1, "chunk_text_len": 1997, "answer_len": 24, "text_norm_len": 8868, "questions_per_context": 11, "llm_models": ["hf.co/NikolayKozloff/Llama-PLLuM-8B-instruct-Q8_0-GGUF:Q8_0"], "embed_models": "/dane/models/sdadas/stella-pl-retrieval", "embed_dim": 1024, "asyncio_semaphore": 10, 
 "timings": {"chunking_s": 0.001, "llm_latency_s": 8.82}}
 
---DELETE FROM qa_results
---WHERE text_id = 669825
---SELECT * FROM answer_embeddings
---WHERE text_id = 669825
-
-
+-------------------------------------------------------------------------
+-- INTERP SQL
 -------------------------------------------------------------------------
 SELECT 
     id_informacji,
@@ -161,25 +175,31 @@ WHERE True
     )
 ORDER BY id_informacji DESC 
 
-----------------------------------------------------------------------------------
-----------------------------------------------------------------------------------
-----------------------------------------------------------------------------------
--- TEST -- TEST -- TEST -- TEST -- TEST -- TEST -- TEST -- TEST -- TEST -- TEST 
-----------------------------------------------------------------------------------
-----------------------------------------------------------------------------------
+
+
+
+
+
+
+
+-------------------------------------------------------------------------
+--------------------- TEST ----------------------------------------------
+-------------------------------------------------------------------------
+-------------------------------------------------------------------------
+-- MAIN TABLES
+-------------------------------------------------------------------------
 DROP TABLE qa_results_test CASCADE
 DROP TABLE answer_embeddings_qa_test
-DROP TABLE answer_embeddings_sa_test
 DROP TABLE stat_results_test
 
 TRUNCATE TABLE qa_results_test CASCADE
 TRUNCATE TABLE answer_embeddings_qa_test
-TRUNCATE TABLE answer_embeddings_sa_test
 TRUNCATE TABLE stat_results_test
 
+DROP TABLE answer_embeddings_sa_test
+TRUNCATE TABLE answer_embeddings_sa_test
 
-
--- QA results per chunk/question/model
+-- TEST: MAIN TABLE!!! QA results per chunk/question/model
 CREATE TABLE IF NOT EXISTS qa_results_test (
   id BIGSERIAL NOT NULL PRIMARY KEY,
   text_id INTEGER NOT NULL, -- id_informacji
@@ -198,8 +218,8 @@ CREATE TABLE IF NOT EXISTS qa_results_test (
   updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL 
 );
 
--------------------------------------------------------------------------
--- Embeddings of answers for retrieval/RAG
+
+-- TEST: Embeddings of answers for retrieval/RAG
 CREATE TABLE IF NOT EXISTS answer_embeddings_qa_test (
   id BIGSERIAL NOT NULL PRIMARY KEY,
   qa_results_id BIGINT NOT NULL REFERENCES qa_results_test(id) ON DELETE CASCADE,
@@ -213,6 +233,26 @@ CREATE TABLE IF NOT EXISTS answer_embeddings_qa_test (
   created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL,
   updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
+
+
+-- TEST: Statistics
+CREATE TABLE IF NOT EXISTS stat_results_test (
+  id BIGSERIAL PRIMARY KEY,
+  text_id INTEGER NOT NULL,
+  tax_type TEXT NOT NULL,
+  type_results TEXT NOT NULL, -- a piece of python code
+  results JSON NOT NULL, -- in general timings in seconds !!! (_s)
+  created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL,
+  updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL
+);
+
+-------------------------------------------------------------------------
+-- TEST OPTIONAL (without REFERENCES qa_results(id) ON DELETE CASCADE)
+-- CHANGE sa to vat, cit, pit ?????
+-- GOAL is SPLITED BY tax_type AND INSERT INTO answer_embeddings_qa_test
+-------------------------------------------------------------------------
+-- DROP TABLE answer_embeddings_sa_test
+-- TRUNCATE TABLE answer_embeddings_sa_test
 
 CREATE TABLE IF NOT EXISTS answer_embeddings_sa_test (
   id BIGSERIAL NOT NULL PRIMARY KEY,
@@ -229,50 +269,35 @@ CREATE TABLE IF NOT EXISTS answer_embeddings_sa_test (
 );
 
 
-CREATE TABLE IF NOT EXISTS stat_results_test (
-  id BIGSERIAL PRIMARY KEY,
-  text_id INTEGER NOT NULL,
-  tax_type TEXT NOT NULL,
-  type_results TEXT NOT NULL, -- a piece of python code
-  results JSON NOT NULL, -- in general timings in seconds !!! (_s)
-  created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL,
-  updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL
-);
+-------------------------------------------------------------------------
+-- SQL TESTS
+-------------------------------------------------------------------------
+--- SELECT
 
-
-
-SELECT * FROM qa_results_test 
-WHERE question_id = 4
-ORDER BY id DESC
-
-
+SELECT * FROM qa_results_test ORDER BY id DESC
 SELECT * FROM answer_embeddings_qa_test ORDER BY id DESC
-
-INSERT INTO  answer_embeddings_sa_test
-
-
-SELECT * FROM answer_embeddings_qa_test
-SELECT * FROM answer_embeddings_sa_test
-
-
-
-
 SELECT * FROM stat_results_test ORDER BY id DESC
 
+SELECT * FROM answer_embeddings_sa_test ORDER BY id DESC 
+
+SELECT 
+    text_id,
+    tax_type,
+    type_results,
+    json_extract_path_text(results,'chunks') AS chunks, 
+    json_extract_path_text(results,'text_norm_len') AS text_norm_len ,
+    json_extract_path_text(results,'timings', 'llm_latency_s') AS llm_latency_s ,
+    json_extract_path_text(results,'timings', 'total_s') AS total_s  
+FROM stat_results_test
 
 
+-- INSERT INTO answer_embeddings_sa_test
 WITH t0 AS (
 SELECT * FROM answer_embeddings_qa_test
 WHERE type_text='chunk' 
 )
-
 INSERT INTO answer_embeddings_sa_test
 SELECT * FROM t0 AS ta
-WHERE qa_results_id = ANY(SELECT MIN(qa_results_id) 
-FROM t0 AS tb WHERE ta.text_id = tb.text_id)
-
-
-SELECT * FROM answer_embeddings_sa_test
-
+WHERE qa_results_id = ANY(SELECT MIN(qa_results_id) FROM t0 AS tb WHERE ta.text_id = tb.text_id)
 
 
